d886586d3dfea11f3946426e11f0b5e0470747e1
yesmar12 <ramsey12345@pm.me>
2021-02-12 17:49:37 -0600

    initial commit with spring intitilizer

---
+++ plantorchestrator/src/main/java/com/ramsey/plantorchestrator/PlantorchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.ramsey.plantorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantorchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantorchestratorApplication.class, args);
+	}
+
+}
---
+++ plantorchestrator/src/test/java/com/ramsey/plantorchestrator/PlantorchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramsey.plantorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantorchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
